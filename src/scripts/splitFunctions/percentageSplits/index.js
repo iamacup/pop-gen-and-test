401453399ee436a716a5a488834e736d44a438c4
Megan Field <megan@sliips.com>
2018-06-21 14:58:26 +0100

    Fix percentages

--- src/scripts/splitFunctions/percentageSplits/index.js
+++ src/scripts/splitFunctions/percentageSplits/index.js
@@ -1,97 +1,112 @@
 
-const _ = require('lodash');
-const colors = require('colors');
+const getPercentage = (answers) => {
+  // Finding the percentage to work from. total becomes the first percentage, then adds on next percentage.
+  // Ex/ if total = 90 and the random number is 95, if the next percentage is 10, total will become 100
+  // and so as 95 < 100 the answer will be the element in answers that has the percentage/weight of 10.
+  let total = 0;
+  const finalAnswers = [];
+  const randomNumber = Math.floor(Math.random() * 100) + 0;
 
-const { getRandomArbitrary } = require('../../randomFunctions');
+  answers.forEach((element) => {
+    total += element.split;
+    if (randomNumber < total) finalAnswers.push(element);
+  });
+  return finalAnswers[0];
+};
+
+const gettingAllValues = (answers, options, count) => {
+  // adding a random option into the answers to hold the remaining percentage.
+  let newAnswers = [];
+  const randomIndex = Math.floor(Math.random() * (options.length - 1)) + 0;
+  if (count < 100) {
+    answers.push({ answer: options[randomIndex], split: 100 - count });
+    newAnswers = answers;
+  } else {
+    answers.forEach((element) => {
+      const newElement = element;
+      newElement.split = (element.split / count) * 100;
+      newAnswers.push(newElement);
+    });
+  }
+  return newAnswers;
+};
 
 const handlePercentageSplits = async (question, config, friendlyName, answer, localInterface, step) => {
-  // we have to create an 'output' here which is initially a copy of the options
-  const out = [];
+  let finalAnswer = {
+    drawData: null,
+    optionID: null,
+    optionValue: null,
+    order: -1,
+    validated: false,
+  };
 
-  // we take a look and see what we do based on the sub type
-  if (typeof config[friendlyName] !== 'undefined' && config[friendlyName].subType === 'pick') {
-    config[friendlyName].split.forEach((value) => {
-      out.push({
-        drawData: null,
-        optionID: null,
-        optionValue: value.lookup,
-        order: -1,
-        validated: false,
+  let answers = [];
+  const options = [];
+
+
+  if (typeof config[friendlyName] !== 'undefined' && config[friendlyName].subType === 'lookup') {
+    // Populating options with left over options, and answers with real answer data.
+    question.parts[friendlyName].options.forEach((option) => {
+      config[friendlyName].split.forEach((element) => {
+        if (element.lookup.toUpperCase() !== option.optionValue.toUpperCase()) options.push(option);
+        else answers.push({ answer: option, split: element.split });
       });
     });
+
+    let count = 0;
+    answers.forEach((elem) => { count += elem.split; });
+
+    let answersArray = Object.assign([], answers);
+    if (count < 100) answersArray = gettingAllValues(answers, options, count);
+
+    answersArray.sort((a, b) => b.split - a.split);
+    finalAnswer = getPercentage(answersArray).answer;
+  } else if (typeof config[friendlyName] !== 'undefined' && config[friendlyName].subType === 'pick') {
+    // Populating options array, and checking wether they are all weighted the same or not.
+    const checkingAnswers = [];
+    let value;
+    const array = Object.assign([], config[friendlyName]).sort((a, b) => a.split - b.split);
+    array.split.forEach((option, i) => {
+      if (i === 0) value = option.split;
+      if (option.split > value) checkingAnswers.push(option);
+      else options.push(option);
+    });
+
+    if (checkingAnswers.length === 0) {
+      // if all the options have the same weight.
+      answers = Object.assign([], options);
+      const randomNumber = Math.floor(Math.random() * (options.length - 1)) + 0;
+      finalAnswer.optionValue = answers[randomNumber].lookup;
+    } else {
+      // if the options are all weighted differently.
+      answers = Object.assign([], checkingAnswers.sort((a, b) => b.split - a.split));
+
+      let count = 0;
+      answers.forEach((elem) => { count += elem.split; });
+
+      let answersArray = Object.assign([], answers);
+      if (count < 100) answersArray = gettingAllValues(answers, options, count);
+      else if (count > 100) answersArray = gettingAllValues(answers, options, count);
+
+      const optionValueFunction = getPercentage(answersArray);
+      if (optionValueFunction.lookup === undefined) finalAnswer.optionValue = optionValueFunction.answer.lookup;
+      else finalAnswer.optionValue = optionValueFunction.lookup;
+    }
   } else {
-    question.parts[friendlyName].options.forEach((value) => {
-      out.push(_.assign({}, value));
-    });
+    // When the config[friendlyName] is undefined.
+    answers = Object.assign([], question.parts[friendlyName].options);
+    const randomNumber = Math.floor(Math.random() * (answers.length - 1)) + 0;
+    finalAnswer = answers[randomNumber];
   }
 
-  // some variables
-  let split = [];
-  let total = 0;
-
-  if (typeof config[friendlyName] !== 'undefined') {
-  // we validate that the total for the config split is not more than 100
-    ({ split } = config[friendlyName]);
-
-    split.forEach((value) => { total += value.split; });
-
-    if (total > 100) {
-      // return Promise.reject(new Error(`Invalid percentage totals: ${total}`));
-    }
-
-    // we attatch range values here where they are specified
-    total = 0;
-
-    split.forEach((value) => {
-      let found = false;
-
-      out.forEach((value2, index) => {
-        if (value.lookup.toUpperCase() === value2.optionValue.toUpperCase()) {
-          out[index].startRange = total;
-          out[index].endRange = (total + value.split) - 1;
-          total += value.split;
-          found = true;
-        }
-      });
-
-      if (found === false) {
-        console.log(`could not match ${value.lookup} in step${step}`.red);
-      }
-    });
-  }
-
-  // here total contains the ammount of assigned percentage
-  // we now setup a variable to work out how many more things there are to spread the remaining percentage accross
-  const remainingItemsCount = out.length - split.length;
-  const incrementAmmount = (100 - total) / remainingItemsCount;
-
-  // we then make up the ones that are not specified
-  out.forEach((value, index) => {
-    if (typeof value.startRange === 'undefined') {
-      out[index].startRange = total;
-      out[index].endRange = (total + incrementAmmount);// - 0.00000000000001;
-      total += incrementAmmount;
-    }
-  });
-
-  // then we pick the value
-  let pickValue = null;
-  const pickedNum = getRandomArbitrary(1, 100);
-
-  out.forEach((value) => {
-    if (pickedNum >= value.startRange && pickedNum <= value.endRange) {
-      pickValue = value;
-    }
-  });
-
-  if (pickValue === null) {
+  // checking that finalAnswer is not null.
+  if (finalAnswer.optionValue === null) {
     console.log(`picking last - percentage split - ${step} - ${friendlyName}`);
-    pickValue = out[out.length - 1];
+    finalAnswer = answers[0];
   }
 
   // answer it and get any followon stuff
-  const followon = await answer.addAnswer(question.questionID, pickValue.optionID, pickValue.optionValue, friendlyName, localInterface, step);
-
+  const followon = await answer.addAnswer(question.questionID, finalAnswer.optionID, finalAnswer.optionValue, friendlyName, localInterface, step);
   return followon;
 };
 
